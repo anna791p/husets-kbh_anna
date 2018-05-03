9d53698afd76f7f0a7e0d0b92c3fd741edddaf9b
Annabelle <32454421+anna791p@users.noreply.github.com>
2018-05-03 21:38:50 +0200

    menu implemented and working, needs styling

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ function showSingleEvent(gEvent){
     //console.log(gEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
     let template = document.querySelector("#bgetemp").content;
     let clone = template.cloneNode(true);
+
+    let day = gEvent.acf.date.substring(6, 8);
+    let month = gEvent.acf.date.substring(4, 6);
+    let year = gEvent.acf.date.substring(0, 4);
+
     clone.querySelector("h1").textContent = gEvent.title.rendered;
     clone.querySelector(".descript").innerHTML = gEvent.content.rendered;
-    clone.querySelector(".date").textContent = gEvent.acf.date;
+
+
+    clone.querySelector(".date").textContent = day + "/" + month + "/" + year; // gEvent.acf.date;
+
+
     clone.querySelector(".time").textContent = gEvent.acf.time;
     clone.querySelector(".venue").textContent = gEvent.acf.venue;
     clone.querySelector(".price").textContent = gEvent.acf.price;
@@ -34,8 +43,6 @@ function showSingleEvent(gEvent){
     gamelist.appendChild(clone);
 }
 fetchGameEvents();
-
-
 
 
 
@@ -60,12 +67,15 @@ function showSingleItem(someFood){
     //clone.querySelctor(".descript").innerHTML = someFood.content.rendered;
     clone.querySelector(".price span").textContent = someFood.acf.price;
 
+
     let fooddrinklist = document.querySelector("#fooddrinklist");
     fooddrinklist.appendChild(clone);
 
 
 }
 fetchEdibles();
+
+
 
 
 
